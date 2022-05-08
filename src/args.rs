59942dad35e1e6a726655cf7ebf8723cf498137f
Dylan Oh <dylanoh@Dylans-MacBook-Pro.local>
2022-05-08 22:46:47 +0800

    Added find_image_from_path function

--- src/args.rs
+++ src/args.rs
@@ -1,10 +1,22 @@
-fn get_nth_Arg(n: usize) -> String {
-    std::env::args().nth(n).unwrap();
+fn get_nth_arg(n: usize) -> String {
+    std::env::args().nth(n).unwrap()
 }
 
 // Remember to put pub access modifier so that others can use this struct
+#[derive(Debug)]
 pub struct Args {
     pub image_1: String,
     pub image_2: String,
     pub output: String
+}
+
+impl Args {
+    pub fn new() -> Self {
+        Args {
+            // index 0 for the iterator is the path to the binary
+            image_1: get_nth_arg(1),
+            image_2: get_nth_arg(2),
+            output: get_nth_arg(3)
+        }
+    }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,32 @@
 // declare the args file as module with mod keyword, so that it can be imported and used
 mod args;
+use std::{io::BufReader, fs::File};
+use image::{io::Reader, DynamicImage, ImageFormat};
 use args::Args;
 
-fn main() {
-    let args = Args {
-        image_1: String::new(),
-        image_2: String::new(),
-        output: String::new()
-    };
-    println!("Hello, world!");
+enum ImageDataErrors {
+    DifferentImageFormats
+}
+
+fn main() -> Result<(), ImageDataErrors> {
+    let args = Args::new();
+    let (image_1, image_format_1) = find_image_from_path(args.image_1);
+    let (image_2, image_format_2) = find_image_from_path(args.image_2);
+    
+    if image_format_1 != image_format_2 {
+        return Err(ImageDataErrors::DifferentImageFormats);
+    }
+    Ok(())
+
+}
+
+fn find_image_from_path(path: String) -> (DynamicImage, ImageFormat) {
+    let image_reader: Reader<BufReader<File>> = Reader::open(path).unwrap();
+    let image_format: ImageFormat = image_reader.format().unwrap();
+    let image: DynamicImage = image_reader.decode().unwrap();
+    (image, image_format)
+}
+
+fn get_smallest_dimension(dim_1: (u32,u32), dim_2: (u32,u32)) -> (u32,u32) {
+    
 }
